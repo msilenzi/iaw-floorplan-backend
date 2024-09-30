ad6059ad5137129e21ee480f8bcfac547af8ec6a
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-09-30 14:35:13 -0300

    update swagger description and version

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,8 @@ async function bootstrap() {
   // Enable swagger
   const config = new DocumentBuilder()
     .setTitle('Floorplan API')
-    .setDescription('IAW - Trabajo Integrador')
-    .setVersion('1.0')
+    .setDescription('IAW - Trabajo Integrador. Manuel Silenzi')
+    .setVersion('0.0')
     .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
