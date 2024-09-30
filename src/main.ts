7939b3065ee4c149f23ed3251046c806ed960253
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-09-30 11:53:50 -0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  // Enable class-validator
   app.useGlobalPipes(new ValidationPipe())
+
+  // Enable swagger
+  const config = new DocumentBuilder()
+    .setTitle('Floorplan API')
+    .setDescription('IAW - Trabajo Integrador')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
 
   await app.listen(3000)
 }
