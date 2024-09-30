5954d4851cc8436547f2c8bbabe9e6460a1cf6f6
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-09-30 15:31:41 -0300

    generate resource usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { MongooseModule } from '@nestjs/mongoose'
+import { UsuariosModule } from './usuarios/usuarios.module'
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/floorplan')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/floorplan'),
+    UsuariosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
